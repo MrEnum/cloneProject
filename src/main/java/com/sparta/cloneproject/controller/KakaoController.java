55c4f9eb7f1ebbb1ac002d9abac4656eb224c419
minssu86 <manager.kim86@gmail.com>
2022-06-18 03:10:41 +0900

    6/17 Kakao Rest Api Key 발행 완료.

--- src/main/java/com/sparta/cloneproject/controller/KakaoController.java
+++ src/main/java/com/sparta/cloneproject/controller/KakaoController.java
@@ -1,4 +1,30 @@
 package com.sparta.cloneproject.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.sparta.cloneproject.service.KakaoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class KakaoController {
+
+    private final KakaoService kakaoService;
+
+    public KakaoController(KakaoService kakaoService){
+        this.kakaoService = kakaoService;
+    }
+//    user/signin/kakao
+//    366bf4df105f7d1fb0c91cb6b4faeba0  <== REST API 키
+//    http://localhost:8080/user/signin/kakao  <== 콜백 주소
+//    https://kauth.kakao.com/oauth/authorize?client_id=366bf4df105f7d1fb0c91cb6b4faeba0&redirect_uri=http://localhost:8080/user/signin/kakao&response_type=code   <== front End href 용
+
+    @GetMapping("user/signin/kakao")
+    public String kakaoLogin(@RequestParam String code) throws JsonProcessingException {
+        kakaoService.kakaoLogin(code);
+        return "redirect:/";  // 로그인 성공 시 처리 방법 협의 필요.
+    }
+
+    // 2-15 25:42
+
 }
--- src/main/java/com/sparta/cloneproject/service/KakaoService.java
+++ src/main/java/com/sparta/cloneproject/service/KakaoService.java
@@ -1,4 +1,106 @@
 package com.sparta.cloneproject.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+@Service
 public class KakaoService {
+
+
+    public void kakaoLogin(String code) throws JsonProcessingException {
+        // 1. "인가 코드"로 "액세스 토큰" 요청
+        // HTTP Header 생성
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        // HTTP Body 생성
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("grant_type", "authorization_code");
+        body.add("client_id", "366bf4df105f7d1fb0c91cb6b4faeba0"); // Rest API Key
+        body.add("redirect_uri", "http://localhost:8080/user/kakao/callback");
+        body.add("code", code);
+
+        // HTTP 요청 보내기
+        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest =
+                new HttpEntity<>(body, headers);
+        RestTemplate rt = new RestTemplate();
+        ResponseEntity<String> response = rt.exchange(
+                "https://kauth.kakao.com/oauth/token",
+                HttpMethod.POST,
+                kakaoTokenRequest,
+                String.class
+        );
+
+        // HTTP 응답 (JSON) -> 액세스 토큰 파싱
+        String responseBody = response.getBody();
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(responseBody);
+        String accessToken = jsonNode.get("access_token").asText();
+
+
+        // 2. 토큰으로 카카오 API 호출
+        // HTTP Header 생성
+        headers.add("Authorization", "Bearer " + accessToken);
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        // HTTP 요청 보내기
+        HttpEntity<MultiValueMap<String, String>> kakaoUserInfoRequest = new HttpEntity<>(headers);
+        response = rt.exchange(
+                "https://kapi.kakao.com/v2/user/me",
+                HttpMethod.POST,
+                kakaoUserInfoRequest,
+                String.class
+        );
+
+        responseBody = response.getBody();
+        jsonNode = objectMapper.readTree(responseBody);
+        Long id = jsonNode.get("id").asLong();
+        String nickname = jsonNode.get("properties")
+                .get("nickname").asText();
+        String email = jsonNode.get("kakao_account")
+                .get("email").asText();
+
+        System.out.println("카카오 사용자 정보: " + id + ", " + nickname + ", " + email);
+    }
+
+
 }
+
+/*
+    카카오 사용자 정보 예시
+    {
+        "id": 1632335751,
+        "properties":
+        {
+            "nickname": "르탄이",
+            "profile_image": "http://k.kakaocdn.net/...jpg",
+            "thumbnail_image": "http://k.kakaocdn.net/...jpg"
+        },
+        "kakao_account":
+        {
+            "profile_needs_agreement": false,
+            "profile":
+            {
+                "nickname": "르탄이",
+                "thumbnail_image_url": "http://k.kakaocdn.net/...jpg",
+                "profile_image_url": "http://k.kakaocdn.net/...jpg"
+            },
+            "has_email": true,
+            "email_needs_agreement": false,
+            "is_email_valid": true,
+            "is_email_verified": true,
+            "email": "letan@sparta.com"
+        }
+    }
+ */
+
+
