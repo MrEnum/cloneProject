66ac037946ab05f708bacbc8c939ba67b3bf488a
MrEnum <103314171+MrEnum@users.noreply.github.com>
2022-06-22 16:54:19 +0900

    포스트 update 완료

--- src/main/java/com/sparta/cloneproject/controller/PostController.java
+++ src/main/java/com/sparta/cloneproject/controller/PostController.java
@@ -1,18 +1,17 @@
 package com.sparta.cloneproject.controller;
 
+import com.sparta.cloneproject.domain.Post;
 import com.sparta.cloneproject.dto.post.PostRequestDto;
 import com.sparta.cloneproject.dto.post.PostResponseDto;
 import com.sparta.cloneproject.repository.PostRepository;
 import com.sparta.cloneproject.security.UserDetailsImpl;
 import com.sparta.cloneproject.service.PostService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class PostController {
@@ -23,21 +22,45 @@ public class PostController {
         this.postService = postService;
         this.postRepository = postRepository;
     }
+
     //게시글 작성
     @PostMapping("/posts/post")
     public void createPost(@AuthenticationPrincipal UserDetailsImpl userDetails,
                            @RequestPart(value = "postDto") PostRequestDto postRequestDto,
                            @RequestPart(value = "file") MultipartFile file//ARC부분에
-                           ){
+    ) {
         Long user = userDetails.getUser().getId();
         postService.createPost(postRequestDto, user, file);
     }
 
     //게시글 전체 조회
     @GetMapping("/posts")
-    public List<PostResponseDto> getAllPosts(){
+    public List<PostResponseDto> getAllPosts() {
         return postService.getAllpost();
     }
-    //게시글 디테일 조회
+    //게시글 카테고리별 전체 조회
 
+    //게시글 디테일 조회
+    @GetMapping("/posts/{postId}")
+    public Optional<Post> getPost(@PathVariable Long postId) {
+        Optional<Post> post = postRepository.findById(postId);
+        return post;
+    }
+
+    //게시글 수정
+    @PutMapping("/post/{postId}")
+    public void updatePost(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                                    @PathVariable Long postId,
+                                    @RequestPart(value = "postDto") PostRequestDto postRequestDto,
+                                    @RequestPart(value = "file") MultipartFile file) {
+        postService.updatePost(postId,userDetails.getUser().getId(), postRequestDto, file);
+
+    }
+
+    //게시글 삭제
+    @DeleteMapping("/post/{postId}")
+    public void deletePost(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                           @PathVariable Long postId) {
+        postService.deletePost(postId, userDetails.getUser().getId());
+    }
 }
